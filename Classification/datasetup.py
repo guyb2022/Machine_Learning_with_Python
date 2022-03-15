3db114455eb502a635c29fff9aa52cb816bf7e6b
Guy B <91497242+guyb2022@users.noreply.github.com>
2022-03-15 11:23:37 +0200

    Update datasetup.py

--- Classification/datasetup.py
+++ Classification/datasetup.py
@@ -1,7 +1,6 @@
 import pandas as pd, matplotlib.pyplot as plt, os, sys, seaborn as sns
 os.getcwd()
 
-# Set up IBM colors
 import colorsetup
 from colorsetup import colors, palette
 sns.set_palette(palette)
@@ -46,38 +45,3 @@ else:
     except FileNotFoundError:
         print(f'\nPickle datafile not found \nRun with argument "s" to read source files')
         sys.exit(1)
-
-#breakpoint()
-
-
-#colors = []
-#colornum = 60
-#for i in [f'Blue {colornum}', f'Teal {colornum}', f'Magenta {colornum}', f'Purple {colornum}', f'Gray {colornum}']:
-#    colors.append(f'#{coloribm[i]}')
-
-#colors = [f'#{coloribm["Blue 40"]}', f'#{coloribm["Purple 40"]}']
-# Set your custom color palette
-#sns.set_palette(sns.color_palette(colors))
-#sns.palplot(sns.color_palette())
-
-
-
-# Columns in original data
-#demo_cols = ['Customer ID', 'Count', 'Gender', 'Age', 'Under 30', 'Senior Citizen',
-#             'Married', 'Dependents', 'Number of Dependents']
-#
-#pop_cols = ['ID', 'Zip Code', 'Population']
-#
-#services_cols = ['Customer ID', 'Count', 'Quarter', 'Referred a Friend',
-#                 'Number of Referrals', 'Tenure in Months', 'Offer', 'Phone Service',
-#                 'Avg Monthly Long Distance Charges', 'Multiple Lines',
-#                 'Internet Service', 'Internet Type', 'Avg Monthly GB Download',
-#                 'Online Security', 'Online Backup', 'Device Protection Plan',
-#                 'Premium Tech Support', 'Streaming TV', 'Streaming Movies',
-#                 'Streaming Music', 'Unlimited Data', 'Contract', 'Paperless Billing',
-#                 'Payment Method', 'Monthly Charge', 'Total Charges', 'Total Refunds',
-#                 'Total Extra Data Charges', 'Total Long Distance Charges', 'Total Revenue']
-
-#status_cols = ['Customer ID', 'Count', 'Quarter', 'Satisfaction Score', 'Customer Status',
-#               'Churn Label', 'Churn Value', 'Churn Score', 'CLTV', 'Churn Category',
-#               'Churn Reason']
